bc268268dd19f536ed8facb0126cf8bc1163faf1
VVchandrahas3 <vvchandrahasreddy7@gmail.com>
2024-09-10 00:01:35 +0530

    Time: 14 ms (93.86%), Space: 27.6 MB (50.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,6 +4,6 @@ public:
         k = k % nums.size();
         reverse(nums.rbegin(),nums.rbegin()+k);
         reverse(nums.rbegin()+k,nums.rend());
-        reverse(nums.rbegin(),nums.rend()); 
+        reverse(nums.begin(),nums.end()); 
     }
 };
