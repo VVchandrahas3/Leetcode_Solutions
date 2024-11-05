feba25f181f81d75fe0dbf55459ad1dafd9cc41f
VVchandrahas3 <vvchandrahasreddy7@gmail.com>
2024-11-05 21:39:50 +0530

    Time: 246 ms (5.12%), Space: 12.7 MB (96.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,41 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Create a vector of pairs to store elements and their original indices
-        vector<pair<int, int>> numWithIndex;
-        for (int i = 0; i < nums.size(); i++) {
-            numWithIndex.push_back({nums[i], i});
-        }
-
-        // Sort the array of pairs based on the first element (the number itself)
-        sort(numWithIndex.begin(), numWithIndex.end());
-
-        // Initialize two pointers
-        int l = 0;
-        int r = numWithIndex.size() - 1;
-
-        // Use two pointers to find the two numbers that add up to the target
-        while (l < r) {
-            int sum = numWithIndex[l].first + numWithIndex[r].first;
-
-            if (sum == target) {
-                // Return the original indices of the two numbers
-                return {numWithIndex[l].second, numWithIndex[r].second};
-            } else if (sum < target) {
-                // Move the left pointer to the right to increase the sum
-                l++;
-            } else {
-                // Move the right pointer to the left to decrease the sum
-                r--;
+    vector<int>n;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                   n.push_back(i);
+                   n.push_back(j);
+                }
             }
-        }
 
-        // Return empty vector if no solution is found (though the problem guarantees one)
-        return {};
+        }
+       return n; 
     }
+    
 };
